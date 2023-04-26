64370f3ef57b07ba6927a197c8dab8d420be9f26
전찬혁 <hyukee0205@gmail.com>
2023-04-26 23:07:52 +0900

    first commit

---
+++ 2.static/src/app/products/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import {notFound} from 'next/navigation';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({params}: Props) {
+  if(params.slug === 'nothing') {
+    notFound();
+  }
+  return <h1>{params.slug}바지 제품 설명 페이지</h1>;
+}
+
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }))
+};
