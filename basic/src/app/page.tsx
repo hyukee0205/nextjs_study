e454e127c3e5bd72c772892154a5783fba835c72
전찬혁 <hyukee0205@gmail.com>
2023-04-29 02:34:12 +0900

    0429

--- basic/src/app/page.tsx
+++ basic/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   console.log('안녕! - 서버');
   return (
     <>
-      <h1>홈페이지다 !</h1>
+      <h1>홈페이지다 ! 버전2</h1>
       <Counter />
       <Image src='https://i.pinimg.com/564x/13/9b/81/139b81b7e9ecd9d26aadf78346ac8f03.jpg' alt='shop' 
       width={400}
