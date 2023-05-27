009df8bcf10306ecb70e0a31924f2dbebc35decd
전찬혁 <hyukee0205@gmail.com>
2023-05-27 12:49:26 +0900

    nextjs csr복습

--- basic_2/src/app/products/page.tsx
+++ basic_2/src/app/products/page.tsx
@@ -1,19 +1,23 @@
+import MeowArticle from '@/components/MeowArticle';
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
+import styles from './page.module.css';
+// export const revalidate = 3;
 
-export default function ProductsPage() {
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-  const products = getProducts();
+export default async function ProductsPage() {
+  const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
