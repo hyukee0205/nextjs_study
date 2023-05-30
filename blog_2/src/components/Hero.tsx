6c0820e3416a33b55e2f75fec57787ce7f7f58f0
전찬혁 <hyukee0205@gmail.com>
2023-05-30 22:59:02 +0900

    blog 복습

--- blog_2/src/components/Hero.tsx
+++ blog_2/src/components/Hero.tsx
@@ -13,9 +13,9 @@ export default function Hero() {
         height={250}
         priority
       />
-      <h2 className='text-3xl font-bold mt-2'>{"Hi, I'm hyuk"}</h2>
-      <h3 className='text-xl font-semibold'>FE 개발자</h3>
-      <p>프론트엔드 개발자 hyuk!</p>
+      <h2 className='text-3xl font-bold mt-2'>{"Hi, I'm Ellie"}</h2>
+      <h3 className='text-xl font-semibold'>Full-stack Engineer</h3>
+      <p>꿈을 코딩하는 사람, 드림코더 엘리</p>
       <Link href='/contact'>
         <button className='bg-yellow-500 font-bold rounded-xl py-1 px-4 mt-2'>
           Contact Me
